a03f77ae2f580c9fe2e9367a7c5dfe2c13119fd8
JoaoVicPugliesi <mrlanguages62@gmail.com>
2025-02-12 20:08:14 -0300

    compressing the videos and trying to add new features

--- js/index.js
+++ js/index.js
@@ -1,9 +1,66 @@
+const square1btn = document.querySelector(".square1btn");
+const square2btn = document.querySelector(".square2btn");
+const square3btn = document.querySelector(".square3btn");
+const square1 = document.querySelector(".square1");
+const square1video = document.querySelector(".square1video");
+const square2 = document.querySelector(".square2");
+const square2video = document.querySelector(".square2video");
+const square3 = document.querySelector(".square3");
+const square3video = document.querySelector(".square3video");
+
+function addExpandListeners() {
+    if (window.innerWidth > 1300) {
+        square1btn.addEventListener('click', () => toggleExpand(square1, square1video));
+        square2btn.addEventListener('click', () => toggleExpand(square2, square2video));
+        square3btn.addEventListener('click', () => toggleExpand(square3, square3video));
+    } else {
+        removeExpandListeners();
+    }
+}
+
+function toggleExpand(square, video) {
+    document.querySelectorAll(".square1, .square2, .square3").forEach(sq => {
+        sq.classList.remove("full");
+    });
+
+    square.classList.add("full");
+    video.classList.add("nohover");
+
+    setTimeout(() => {
+        document.addEventListener("click", closeExpand);
+    }, 100); 
+}
+
+function closeExpand(event) {
+    if (!event.target.closest(".square1, .square2, .square3")) {
+        document.querySelectorAll(".square1, .square2, .square3").forEach(sq => {
+            sq.classList.remove("full");
+        });
+        document.removeEventListener("click", closeExpand);
+    }
+}
+
+function removeExpandListeners() {
+    square1btn.replaceWith(square1btn.cloneNode(true));
+    square2btn.replaceWith(square2btn.cloneNode(true));
+    square3btn.replaceWith(square3btn.cloneNode(true));
+}
+
+addExpandListeners();
+window.addEventListener("resize", addExpandListeners);
+
 document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll('a').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             const targetId = this.getAttribute('href');
             if (targetId.startsWith("#")) {  
                 e.preventDefault();
+                square1.classList.remove('full');
+                square2.classList.remove('full');
+                square3.classList.remove('full');
+                square1video.classList.remove('nohover');
+                square2video.classList.remove('nohover');
+                square3video.classList.remove('nohover');
                 const targetSection = document.querySelector(targetId);
                 if (targetSection) {
                     window.scrollTo({
@@ -38,7 +95,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 }
             }
         });
-    }, { threshold: 0.2 });
+    }, { threshold: 0.5 });
 
     const storeElements = document.querySelectorAll('.store, .productstore');
     storeElements.forEach(el => storeObserver.observe(el));
@@ -81,10 +138,10 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".biobtn2").addEventListener('click', () => {
         bio.innerHTML = `
             <h3 class="highlightname">Luan Lacerda - MMA Highlights</h3>
-            <div>
-                <video class="highlight" src="videos/highlights.mp4" controls autoplay loop></video>
-                <div><a href="#store"><img class="arrow" src="images/down.png" alt=""></a></div>
+            <div class="highlightdiv">
+                <video class="highlight" src="videos/highlights.mp4" poster="images/logo.png" controls autoplay loop></video>
             </div>
+            <div><a href="#store"><img class="arrow" src="images/down.png" alt=""></a></div>
         `;
     });
 
@@ -104,3 +161,4 @@ document.addEventListener("DOMContentLoaded", function () {
         `;
     });
 });
+
